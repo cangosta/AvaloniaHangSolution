f81f19bd235720738a1907ced88178179f6470f2
Luiz Santos <luiz.santos@outsystems.com>
2022-03-25 14:25:02 +0000

    Pointer position bug

--- src/AvaloniaHangProject/Views/MainWindow.axaml.cs
+++ src/AvaloniaHangProject/Views/MainWindow.axaml.cs
@@ -1,15 +1,36 @@
+#nullable enable
+using System.Diagnostics;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
 
 namespace AvaloniaHangProject.Views {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+
+            tab = this.FindControl<TabItem>("Circle");
+            tab.PointerPressed += TabOnPointerPressed;
+            tab.PointerMoved += TabOnPointerMoved;
+            
+
 #if DEBUG
             this.AttachDevTools();
 #endif
         }
+
+        private void TabOnPointerMoved(object? sender, PointerEventArgs e) {
+            Debug.WriteLine("Moved position" + e.GetPosition(this));
+            Debug.WriteLine("Moved current point" + e.GetCurrentPoint(this));
+        }
+
+        private void TabOnPointerPressed(object? sender, PointerPressedEventArgs e) {
+            Debug.WriteLine("Pressed position" + e.GetPosition(this));
+            Debug.WriteLine("Pressed current point" + e.GetCurrentPoint(this));
+        }
+
+        private readonly TabItem tab;
 
         private void InitializeComponent() {
             AvaloniaXamlLoader.Load(this);
